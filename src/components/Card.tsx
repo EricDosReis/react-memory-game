2a29836bc8817f4e1c47b2c9ff1f0622bc5e7dd2
EricDosReis <ericdosreis.personal@gmail.com>
2025-05-13 09:38:32 -0300

    feat: create Card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,40 @@
+import { cn } from "@/lib/utils";
+import { Sparkles } from "lucide-react";
+import { useState } from "react";
+
+const STYLES = {
+  base: "backface-hidden absolute flex h-full w-full items-center justify-center rounded-xl border-2",
+  front: "bg-pink border-white/20",
+  back: "rotate-y-180 border-purple-200 bg-white",
+};
+
+const Card = () => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  return (
+    <button
+      className="relative h-16 w-16 cursor-pointer"
+      type="button"
+      onClick={handleClick}
+    >
+      <div
+        className={cn(
+          "transform-3d duration-250 h-full w-full transition-transform",
+          isFlipped && "rotate-y-180",
+        )}
+      >
+        <div className={cn(STYLES.base, STYLES.front)}>
+          <Sparkles className="h-6 w-6 text-white" />
+        </div>
+
+        {isFlipped && <div className={cn(STYLES.base, STYLES.back)}>🍬</div>}
+      </div>
+    </button>
+  );
+};
+
+export { Card };
--- src/components/Hello.tsx
+++
@@ -1,5 +0,0 @@
-const Hello = () => {
-  return <h1 className="text-xl">Lets dudes</h1>;
-};
-
-export { Hello };
