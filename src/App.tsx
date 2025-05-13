7630c869b776941ced39432b37e38db48e588bc2
EricDosReis <ericdosreis.personal@gmail.com>
2025-05-13 14:30:34 -0300

    feat: create Modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
+
 import { Board } from "@/components/Board";
-import { ScoreBoard } from "./components/ScoreBoard";
+import { Modal } from "@/components/Modal";
+import { ScoreBoard } from "@/components/ScoreBoard";
 
 const App = () => {
+  const [showModal, setShowModal] = useState(true);
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-blue-200">
       <ScoreBoard moves={0} time={0} onRestart={() => {}} />
+
       <Board />
+
+      {showModal && <Modal moves={0} time={0} onRestart={handleCloseModal} />}
     </div>
   );
 };
